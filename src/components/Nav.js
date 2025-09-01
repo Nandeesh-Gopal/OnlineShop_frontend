4e53eb93fa37f234896e1340325ffd261f6eb736
Nandeesh-Gopal <nandeeshgopal26@gmail.com>
2025-09-01 21:22:49 +0530

    added some css for the nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,36 @@
-import {Link} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 import Logout from "./Logout";
-import "../styles/nav.css"
-function Nav(){
-    const token = localStorage.getItem("token")
-    return (
-        <div className="main-container">
-            <nav className='nav-bar'>
-                <h1>Online Shop</h1>
-                <input type='text' placeholder='search'/>
-                <div className='sub-nav'>
-{
-                token ?(
-                <>
-                    <Link to="/">home</Link>
-                    <Link to="/partner">business partner</Link>
-                    <Link to="/cart">cart</Link>
-                    <Logout/>
-                </>
-                ):(
-                <>
-                    <Link to="/">home</Link>    
-                    <Link to='/signup'>signup</Link>
-                    <Link to='/login'>login</Link>
-                </>
-                )}
-                    
-                    
-                </div>
-            </nav>
+import "../styles/nav.css";
+
+function Nav() {
+  const navi = useNavigate();
+  const token = localStorage.getItem("token");
+
+  return (
+    <div className="main-container">
+      <nav className="nav-bar">
+        <h1>Online Shop</h1>
+        <input type="text" placeholder="search" />
+ 
+        <div className="sub-nav">
+          <a onClick={() => navi("/")}>Home</a>
+
+          {token ? (
+            <>
+              <a onClick={() => navi("/partner")}>Business Partner</a>
+              <a onClick={() => navi("/cart")}>Cart</a>
+              <Logout />
+            </>
+          ) : (
+            <>
+              <a onClick={() => navi("/signup")}>Signup</a>
+              <a onClick={() => navi("/login")}>Login</a>
+            </>
+          )}
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
+
 export default Nav;
